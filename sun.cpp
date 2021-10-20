bb32a7024bc804e47cf868e07425e4c676cdf53b
Danilkormilin <64023679+Danilkormilin@users.noreply.github.com>
2021-10-20 12:37:14 +0500

    Update sun.cpp

--- sun.cpp
+++ sun.cpp
@@ -1,5 +1,22 @@
 #include "graphics.h"
+#include "math.h"
 #include "picture.hpp"
 void sun() {
-   
+   int x = 50, y = 50;
+   setfillstyle(SOLID_FILL, YELLOW);
+    setcolor(YELLOW);
+    fillellipse(x, y, 50, 50);
+    double ug = 0;
+    for (double ugol = 0; ugol < acos(-1); ugol += acos(-1) / 20)
+    {
+        setcolor(YELLOW);
+    line(x + 60 * cos(ugol),
+        y + 60 * sin(ugol),
+        x + 100 * cos(ugol),
+        y + 100 * sin(ugol));
+    line(x - 60 * cos(ugol),
+        y - 60 * sin(ugol),
+        x - 100 * cos(ugol),
+        y - 100 * sin(ugol));
+    }
 }

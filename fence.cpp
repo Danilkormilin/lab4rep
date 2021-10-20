7adca2ec3dbf178055aa986e6608a4bcc84450c3
1YaStReB1 <92804295+1YaStReB1@users.noreply.github.com>
2021-10-20 12:40:25 +0500

    Update fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -1,5 +1,14 @@
 #include "graphics.h"
 #include "picture.hpp"
 void fence() {
-   
+    int i;
+    setlinestyle(SOLID_LINE,2,3);
+    setfillstyle(SOLID_FILL,RED);
+    setcolor(RED);
+
+ for( i=0 ; i <10; i++){
+      line(200+20*i, 500, 205+20*i,490);
+      line(205+20*i,490,210+20*i,500);
+      bar(200+20*i,500,210+20*i,600);
+    }
 }

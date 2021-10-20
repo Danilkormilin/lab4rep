0ca5d743b4c36c463769390fec2c83c89ee65657
IlyaBikmeev <68954703+IlyaBikmeev@users.noreply.github.com>
2021-10-20 12:38:30 +0500

    Update man.cpp
    Created function "man" which draws simplified 2D man

--- man.cpp
+++ man.cpp
@@ -1,5 +1,11 @@
 #include "graphics.h"
 #include "picture.hpp"
 void man() {
-   
+   setcolor(BLUE);
+   circle(400, 300, 20);
+   line(400, 320, 400, 400);    //head
+   line(400, 320, 380, 350);    //left arm
+   line(400, 320, 420, 350);    //right arm
+   line(400, 400, 380, 430);    //left leg
+   line(400, 400, 420, 430);    //right leg
 }

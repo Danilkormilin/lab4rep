3a42ef39bd490d19df4366b5abe62e153a726155
IlyaBikmeev <68954703+IlyaBikmeev@users.noreply.github.com>
2021-10-20 13:06:32 +0500

    Update man.cpp
    Опустил человека пониже

--- man.cpp
+++ man.cpp
@@ -2,10 +2,10 @@
 #include "picture.hpp"
 void man() {
    setcolor(BLUE);
-   circle(400, 300, 20);
-   line(400, 320, 400, 400);    //head
-   line(400, 320, 380, 350);    //left arm
-   line(400, 320, 420, 350);    //right arm
-   line(400, 400, 380, 430);    //left leg
-   line(400, 400, 420, 430);    //right leg
+   circle(400, 370, 20);
+   line(400, 390, 400, 470);    //head
+   line(400, 390, 380, 420);    //left arm
+   line(400, 390, 420, 420);    //right arm
+   line(400, 470, 380, 500);    //left leg
+   line(400, 470, 420, 500);    //right leg
 }

26268df4d8946de19f1b5f10303cdc82ed41bd2e
1YaStReB1 <92804295+1YaStReB1@users.noreply.github.com>
2021-10-27 11:38:24 +0500

    Update fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -6,8 +6,8 @@ void fence() {
     setfillstyle(SOLID_FILL,RED);
     setcolor(RED);
 
- for( i=0 ; i <10; i++){
-    int tch[] = {200+20*i, 500, 205+20*i,490,210+20*i,500};
+ for( i=0 ; i <30; i++){
+    int tch[] = {20*i, 500, 5+20*i,490,10+20*i,500};
      fillpoly(3,tch);
       bar(200+20*i,500,210+20*i,600);
     }
